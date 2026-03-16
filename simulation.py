f9281aa7ba2f25060c556d877defca30f54943d2
YosieSYX <e1300499@u.nus.edu>
2026-03-16 00:09:00 -0400

    add arm

---
+++ simulation.py
@@ -0,0 +1,22 @@
+import time
+
+import mujoco as mj
+import mujoco.viewer
+
+model = mj.MjModel.from_xml_path("./model/scene.xml")
+data = mj.MjData(model)
+
+with mujoco.viewer.launch_passive(model, data) as v:
+    while v.is_running():
+        step_start = time.time()
+
+        # Advance simulation by one step
+        mj.mj_step(model, data)
+
+        # Update viewer
+        v.sync()
+
+        # Keep real-time pacing
+        dt = model.opt.timestep - (time.time() - step_start)
+        if dt > 0:
+            time.sleep(dt)
